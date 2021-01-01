5df3b43329f3c2e788fe583da5bf5bdc5bde7978
INTRANET\fjakovljevic <fjakovljevic@cs.hr>
2021-01-01 19:21:45 +0100

    Added app header. Added phonebook options.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewStateService } from './services/view-state.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    public isMobileDevice: boolean = false;
+    public activeOption: string;
+
+    constructor(private viewStateService: ViewStateService, private router: Router) {
+        this.isMobileDevice = this.viewStateService.checkIfMobileResolution();
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.activeOption = this.router.url.substr(1);
+        });
+    }
 }
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsComponentModel } from './contacts.component.model';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.component.html'
+    selector: 'app-contacts',
+    templateUrl: './contacts.component.html'
 })
 export class ContactsComponent {
     private model: ContactsComponentModel;
     public activeViewModel: ContactsComponentModel;
+    testMsg: any;
 
-    constructor() {
+    constructor(private router: Router) {
         this.model = new ContactsComponentModel();
         this.activeViewModel = new ContactsComponentModel();
+        this.testMsg = this.router.url === '/contacts' ? 'Contacts' : 'Favorites';
     }
+}
+
+export class Contact {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumbers: Array<PhoneNumber>;
+    favorite: boolean;
+    imageUrl: string;
+
+    constructor(id: number, firstName: string, lastName: string, email: string, phoneNumbers: Array<PhoneNumber>) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumbers = phoneNumbers;
+    }
+}
+
+export class PhoneNumber {
+    number: string;
+    type: string;
 }
